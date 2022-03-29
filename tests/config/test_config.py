f6be20ee26b10e8e7118e5ebbc5173e6b3b1b8f8
Radek Hrbacek <hrbacek.r@benderrobotics.com>
2022-03-29 14:58:19 +0200

    Version 0.0.4

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -3,7 +3,7 @@ from brock.config.config import Config
 
 def test_full_config():
     config = Config('example_brock.yml')
-    assert str(config.version) == '0.0.3'
+    assert str(config.version) == '0.0.4'
     assert config.project == 'someprojectname'
     assert config.help == 'brock --help message'
 
